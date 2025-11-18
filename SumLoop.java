d5a7702fe92fe9717da63ae35604dd409459b162
Dnyaneshwar-991 <dnyaneshwarghuge991@gmail.com>
2025-11-18 23:29:58 +0530

    Add files via upload
    Factorial number using loop

---
+++ SumLoop.java
@@ -0,0 +1,32 @@
+//  import java.util.*;
+// // public class SumLoop {
+// // public static void main(String[]args){
+// //     Scanner sc = new Scanner(System.in);
+// //     int n = sc.nextInt();
+// //     int sum=0;
+// //     for(int i=0;i<=n;i++)
+// //     {
+// //     sum=sum+i;
+// //     }
+// //     System.out.println(sum);
+// // }
+// // }
+
+
+// // Factorial
+// public class SumLoop{
+ 
+//      public static void main(String []args)
+//      {
+//      Scanner sc = new Scanner(System.in);
+//      int n = sc.nextInt();
+//      int fact=1;
+//      for(int i=1;i<=n;i++)
+//      {
+//         fact=fact*i;
+//       System.out.println("Factorial of "+n+"="+fact);
+      
+//      }
+
+//      }
+// } 
